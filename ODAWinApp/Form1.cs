8118a9db0b38418759ef6123c351ce322d1d23d4
Sheridan Jones <sheridan.jones@ntlworld.com>
2017-03-19 16:13:40 +0000

    Initial

--- ODAWinApp/Form1.cs
+++ ODAWinApp/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace ODAWinApp
 {
