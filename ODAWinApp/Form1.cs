fd18cc3d071ae1cd39ab8b81355076c30605d745
Sheridan Jones <sheridan.jones@ntlworld.com>
2017-03-19 16:57:24 +0000

    Added connection info

--- ODAWinApp/Form1.cs
+++ ODAWinApp/Form1.cs
@@ -18,5 +18,25 @@ namespace ODAWinApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string oradb = "Data Source = orcl; user id = hr; password = hr;";
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select department_name from departments where department_id = 10";
+            cmd.CommandType = CommandType.Text;
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            dr.Read();
+
+            label1.Text = dr.GetString(0);
+
+            conn.Dispose();
+        }
     }
 }
